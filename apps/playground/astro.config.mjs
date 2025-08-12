b73be7bb68539bc9e5398f21f509a8cc3336eab8
Armin Imsirevic <armin.imsirevic@outlook.com>
2025-08-12 21:04:51 +0200

    working

--- apps/playground/astro.config.mjs
+++ apps/playground/astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    ssr: {
+      noExternal: ['@imsirevic/ui']
+    },
     plugins: [tailwindcss()]
   }
 });
